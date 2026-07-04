a3d0c625e71f575c9b1b20a259cd17e65f96054a
potonnickpridumayu <marvin.unior@gmail.com>
2026-07-04 11:03:49 +0500

    fix: node polyfills plugin for @ton/core\

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
-  build: { outDir: 'dist' }
+  plugins: [react(), nodePolyfills()],
 })
